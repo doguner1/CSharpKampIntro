50647c93b3e71a13928a0d70cd30b6a9fd3dd0ba
Dogukan GUNER <d.gunr@yaani.com>
2022-05-20 15:16:26 +0300

    Add project files

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using GenericIntro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generic olduğundan düz new ile bu sınıf newlenemez.
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Hello");
+        }
+    }
+}
--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -9,9 +9,23 @@ namespace Koleksiyonlar
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-             
+            List<string> kelimeler = new List<string> {"Doğu","Güner","İnönü","Malatya" };
+            kelimeler.Add("Yeşilyurt");
+
+            Console.WriteLine(kelimeler[3]);
+            Console.WriteLine(kelimeler[4]);
+            kelimeler.Remove("Doğu");
+            Console.WriteLine(kelimeler[0]);
+
         }
     }
 }
+
+
+
+//ssdfsdf
+//sdfsdfsdf    ==> ctlr k , ctrl c yapınca seçtiğin kısmı yoruma alıyor. Toplu yorum. Geri almak için ctrl k , ctrl u.
+//sdfsdfdsf
